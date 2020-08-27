325f22f178cff4d01bbd67cbcc001e25633e2c28
jberrio <jberrio@transfiriendo.com>
2020-08-27 18:08:39 -0500

    Se adiciona rutas http basicas en los controladores de la webapi

---
+++ RouletteApi/Controllers/BetController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouletteApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class BetController : ControllerBase
+    {
+        [Route("bets/{idroulette}")]
+        [HttpGet]
+        public IActionResult getBetsByRouletteController(int idroulette)
+        {
+            return Ok(idroulette);
+        }
+        [Route("betroulette")]
+        [HttpPost]
+        public IActionResult BetOnRouletteController()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
---
+++ RouletteApi/Controllers/RouletteController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouletteApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RouletteController : ControllerBase
+    {
+        [Route("roulettes")]
+        [HttpGet]
+        public IActionResult RouleteListController()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+        [Route("addroulette")]
+        [HttpPost]
+        public IActionResult saveRouletteController()
+        {
+            return Ok(new string[] {  "saludo", "Hola" });
+        }
+        [Route("closeroulette/{idroulette}")]
+        [HttpPut]
+        public IActionResult closeRouletteController(int idroulette)
+        {
+            return Ok(new string[] { "saludo", "Hola" });
+        }
+        [Route("openroulette/{idroulette}")]
+        [HttpPut]
+        public IActionResult openRouletteController(int idroulette)
+        {
+            return Ok(new string[] { "saludo", "Hola" });
+        }
+    }
+}
